f4b0863f9fa1373c834fcd11df3ac56cf182f0c9
Alex Akoopie <alexakoopie@gmail.com>
2024-05-24 04:06:56 -0700

    Updated myCommands.py
    Refactored code.

--- src/myCommands.py
+++ src/myCommands.py
@@ -1,10 +1,9 @@
 #======Libraries======
 import io
 import os
-
 import random
 import re
-from typing import Literal, Optional
+from typing import List, Literal, Mapping, Optional
 
 import discord
 from discord import app_commands
@@ -13,36 +12,35 @@ from discord.ext.commands import Context, Greedy
 
 from commands.TicTacToe import TicTacToe
 
-#Sync Command
+# Syncs slash commands to Discord's servers
 class SyncCommand(commands.Cog):
-    #Constructor
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
 
-    #Syncs slash commands to Discord's servers
-    @commands.command(hidden = True)
+    @commands.command(hidden=True)
     @commands.guild_only()
     @commands.is_owner()
-    async def sync(self, ctx: Context, guilds: Greedy[discord.Object] = None, spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    async def sync(self, ctx: Context, guilds: Greedy[discord.Object]=None, spec: Optional[Literal["~", "*", "^"]]=None) -> None:
+        bot: commands.Bot = ctx.bot
         if not guilds:
             if spec == "~":
-                synced = await ctx.bot.tree.sync(guild = ctx.guild)
+                synced = await bot.tree.sync(guild=ctx.guild)
             elif spec == "*":
-                ctx.bot.tree.copy_global_to(guild = ctx.guild)
-                synced = await ctx.bot.tree.sync(guild = ctx.guild)
+                bot.tree.copy_global_to(guild=ctx.guild)
+                synced = await bot.tree.sync(guild=ctx.guild)
             elif spec == "^":
-                ctx.bot.tree.clear_commands(guild = ctx.guild)
-                await ctx.bot.tree.sync(guild = ctx.guild)
+                bot.tree.clear_commands(guild=ctx.guild)
+                await bot.tree.sync(guild=ctx.guild)
                 synced = []
             else:
-                synced = await ctx.bot.tree.sync()
+                synced = await bot.tree.sync()
             
             return await ctx.send(f"SYNCED {len(synced)} COMMANDS {'GLOBALLY!' if spec is None else 'TO THE CURRENT GUILD!'}")
         
         ret = 0
         for guild in guilds:
             try:
-                await ctx.bot.tree.sync(guild = guild)
+                await bot.tree.sync(guild=guild)
             except discord.HTTPException:
                 pass
             else:
@@ -50,49 +48,56 @@ class SyncCommand(commands.Cog):
             
         await ctx.send(f"SYNCED THE TREE TO {ret}/{len(guilds)}")
 
-#Subclassed Help Command
+# Subclassed Help Command
 class MyHelp(commands.MinimalHelpCommand):
-    #Constructor
+    # Constructor
     def __init__(self):
         super().__init__(
             command_attrs = {"help": "Alex Bot displays a list of commands!"}
         )
     
-    #Help Command (prefix)
-    async def send_bot_help(self, mapping):
-        embed = discord.Embed(title = "ALEX BOT INFORMATION", description = "Created by abacus_paradox", color = 0xf600ff)
-        author_file = discord.File(os.path.join("assets", "server_icon.png"), filename = "server_icon.png")
-        embed.set_author(name = "SUPPORT SERVER", url = "https://discord.com/invite/9zHWtZr", icon_url = "attachment://server_icon.png")
-        thumbnail_file = discord.File(os.path.join("assets", "avatar.png"), filename = "avatar.png")
-        embed.set_thumbnail(url = "attachment://avatar.png")
-        footer_file = discord.File(os.path.join("assets", "help_icon.png"), filename = "help_icon.png")
+    # Help Command (prefix)
+    async def send_bot_help(self, mapping: Mapping[Optional[commands.Cog], List[commands.Command]]):
+        author_file = discord.File(os.path.join("assets", "server_icon.png"), filename="server_icon.png")
+        thumbnail_file = discord.File(os.path.join("assets", "avatar.png"), filename="avatar.png")
+        footer_file = discord.File(os.path.join("assets", "help_icon.png"), filename="help_icon.png")
+
+        embed = discord.Embed(title="ALEX BOT INFORMATION", description="Created by abacus_paradox", color=0xf600ff)
+        embed.set_author(name="SUPPORT SERVER", url="https://discord.com/invite/9zHWtZr", icon_url="attachment://server_icon.png")
+        embed.set_thumbnail(url="attachment://avatar.png")
+
         for cog, commands in mapping.items():
             filtered = await self.filter_commands(commands)
             command_docs = [f"`{self.get_command_signature(c)}`\n{c.help}\n" for c in filtered]
             if command_docs:
                 cog_name = getattr(cog, "qualified_name", "No Category")
-                embed.set_footer(text = re.sub(r"`", r"", "\n".join(command_docs)), icon_url = "attachment://help_icon.png") if not cog else embed.add_field(name = cog_name, value = re.sub(r" `", r"`", "\n".join(command_docs)), inline = True)
-        await self.context.send(files = (author_file, thumbnail_file, footer_file), embed = embed)
+                if not cog:
+                    embed.set_footer(text=re.sub(r"`", r"", "\n".join(command_docs)), icon_url="attachment://help_icon.png")
+                else:
+                    embed.add_field(name=cog_name, value=re.sub(r" `", r"`", "\n".join(command_docs)), inline=True)
+        await self.context.send(files=(author_file, thumbnail_file, footer_file), embed=embed)
 
 #======General Commands======
-class GeneralCommands(commands.Cog, name = "GENERAL COMMANDS"):
-    #Constructor
+class GeneralCommands(commands.Cog, name="GENERAL COMMANDS"):
+    # Constructor
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
         self.bot.help_command = MyHelp()
     
-    #Help Command (slash)
+    # Help Command (slash)
     @app_commands.command(name = "help")
     async def slash_help(self, interaction: discord.Interaction, *, command: Optional[str]):
         """Alex Bot displays a list of commands!"""
+
         my_help = MyHelp()
         my_help.context = ctx = await Context.from_interaction(interaction)
-        await my_help.command_callback(ctx, command = command)
+        await my_help.command_callback(ctx, command=command)
 
-    #Hello Command
+    # Hello Command
     @commands.hybrid_command()
     async def hello(self, ctx: Context):
         """Alex Bot greets you!"""
+
         responses = [
             "BEEP BEEP BOOP! I'M ALEX BOT!",
             "ALEX BOT HERE!",
@@ -102,33 +107,37 @@ class GeneralCommands(commands.Cog, name = "GENERAL COMMANDS"):
         response = random.choice(responses)
         await ctx.send(response)
     
-    #Ping Command
+    # Ping Command
     @commands.hybrid_command()
     async def ping(self, ctx: Context):
         """Pings Alex Bot!"""
+
         await ctx.send("PONG!")
 
-    #Tyler Command
+    # Tyler Command
     @commands.hybrid_command()
     async def tyler(self, ctx: Context):
         """Alex Bot displays a Tyler of his choice!"""
+        tyler_folder_path: str = self.bot.tyler_folder_path
+
         messages_basic = [
-        "THIS IS TYLER!",
-        "HELLO TYLER!",
-        "YOU LOOK CUTE, TYLER!",
-        "OH TYLER...",
-        "WHAT ARE YOU DOING, TYLER?"
+            "THIS IS TYLER!",
+            "HELLO TYLER!",
+            "YOU LOOK CUTE, TYLER!",
+            "OH TYLER...",
+            "WHAT ARE YOU DOING, TYLER?"
         ]
 
-        #Writes message and uploads file
-        my_files = os.listdir(self.bot.tyler_folder_path)
+        # Writes message and uploads file
+        my_files = os.listdir(tyler_folder_path)
         pick_pic = random.choice(my_files)
-        await ctx.send(random.choice(messages_basic), file = discord.File(os.path.join(self.bot.tyler_folder_path, pick_pic)))
+        await ctx.send(random.choice(messages_basic), file=discord.File(os.path.join(tyler_folder_path, pick_pic)))
     
-    #Send Command
+    # Send Command
     @commands.hybrid_command()
     async def send(self, ctx: Context, user: discord.User, message: str):
         """Alex Bot sends a message to a user!"""
+
         await ctx.send(f"SENDING MESSAGE TO {user.name}...")
         try:
             await user.send(message)
@@ -136,14 +145,14 @@ class GeneralCommands(commands.Cog, name = "GENERAL COMMANDS"):
             print(e)
             await ctx.send("ERROR! ERROR! CAN'T DO IT!!!")
     
-    #Avatar Command
+    # Avatar Command
     @commands.hybrid_command()
     async def avatar(self, ctx: Context, user: discord.User = commands.Author):
         """Alex Bot displays the profile picture of a user!"""
 
         buffer = io.BytesIO()
-        await user.display_avatar.save(buffer) #Saves avatar in a buffer
-        avatar = discord.File(buffer, filename = f"{user.name}_avatar.gif") if user.display_avatar.is_animated() else discord.File(buffer, filename = f"{user.name}_avatar.webp")
+        await user.display_avatar.save(buffer) # Saves avatar in a buffer
+        avatar = discord.File(buffer, filename=f"{user.name}_avatar.gif") if user.display_avatar.is_animated() else discord.File(buffer, filename=f"{user.name}_avatar.webp")
         await ctx.send(file=avatar)
     
     # Color Command
@@ -183,7 +192,7 @@ class GeneralCommands(commands.Cog, name = "GENERAL COMMANDS"):
 
         await TicTacToe().run(ctx, user)
 
-#Commands Setup
+# Commands Setup
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(SyncCommand(bot))
     await bot.add_cog(GeneralCommands(bot))
